4097872df6d478ef63a5026dcda2cdfc3838442d
unknown <aadeye302@caledonian.ac.uk>
2026-02-15 21:51:15 +0000

    docker compose

--- producer/extract.py
+++ producer/extract.py
@@ -14,11 +14,12 @@ def connect_to_api():
             response = requests.get(url, headers=headers, params=querystring)
             response.raise_for_status()
             data = response.json()
-            logger.info("Stocks successfully loaded")
+            logger.info(f"{stocks[stock]} Stocks successfully loaded")
             json_response.append(data)
         except requests.exceptions.RequestException as e:
             logger.error(f"Error on stock: {e}")
             break 
+    return json_response
 
 
 
@@ -28,7 +29,9 @@ def extract_json(response):
     for data in response:
         symbol = data['Meta Data']['2. Symbol']
         for date_str, metrics in data['Time Series (5min)'].items():
-            record = { "symbol": symbol, "date": date_str, "open": metrics["1. open"], "close":
-                      metrics["2. high"], "low": metrics["3. low"]}
+            record = { "symbol": symbol, "date": date_str, "open": metrics["1. open"], "close": metrics["4. close"],
+                      "high":metrics["2. high"], "low": metrics["3. low"]}
             records.append(record)
     return records
+
+
